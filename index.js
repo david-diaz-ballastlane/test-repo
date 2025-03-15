41bc51335e43e8ebadc615e38c31e9852046d62f
ddiazghub <dedemoya@uninorte.edu.co>
2025-03-15 14:05:43 -0500

    commit

--- index.js
+++ index.js
@@ -9,4 +9,8 @@ app.get('/hello', (req, res) => {
   res.status(200).send('Hello World!!!')
 })
 
+app.get('/bye', (req, res) => {
+  res.status(200).send('Bye World!!!')
+})
+
 app.listen(PORT, () => console.log('Server running on port', PORT))
